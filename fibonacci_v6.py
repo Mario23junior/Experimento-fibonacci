11e9ecf1716ddcb8940a67f35bd2ab83d4c83366
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2023-01-28 06:42:05 -0300

    Inserindo if em uma unica linha.

---
+++ fibonacci_v6.py
@@ -0,0 +1,10 @@
+#!/usr/loca/bin/python3
+
+def fibonacci(quantidade, sequencia=(0, 1)):
+    return sequencia if len(sequencia) == sequencia else \
+        fibonacci(quantidade, sequencia + (sum(sequencia[-2:]),))
+
+
+if __name__ == '__main__':
+    for fib in fibonacci(20):
+        print(fib)
